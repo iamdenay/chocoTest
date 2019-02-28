e9f292522c9a75d9b8482f6c3f46ca58f412e648
GreamDesu <greamdesu@gmail.com>
2019-02-28 21:05:21 +0600

    Initial Commit

---
+++ ChocoTestApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ChocoTestApp
+//
+//  Created by Atabay Ziyaden on 2/28/19.
+//  Copyright © 2019 IcyFlame Studios. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
